6a2b4f344dea1339988d6ea74d38015b4c61579a
Rinatik Fathulin <roma.tankin@gmail.com>
2019-05-19 15:23:50 +0300

    Добавьте файлы проекта.

---
+++ MyLibrary/Book.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    class Book
+    {
+        private DateTime year;
+        private string nameAuthor;
+        private string title;
+        private string state;
+        public int SetInfBook();
+        public int ShowInfBook();
+    }
+}
